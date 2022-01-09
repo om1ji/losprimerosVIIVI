e4699aef13aac5e495052bc4a77facae7e8e87ae
om1ji <om1ji.music@gmail.com>
2022-01-09 13:59:57 +0500

    Update subs.py

--- src/subs.py
+++ src/subs.py
@@ -1,6 +1,8 @@
-from requests import get
+from requests import post
+
+host = input("Host: ")
 
 channels = ["UC6qQOTx9LuKMC5p2dbjmSRg", "UCLXM6lFu1s7MjInzUk5bfNA"]
 
 for i in channels:
-    print(get(f'https://pubsubhubbub.appspot.com/subscribe?hub.callback=http://92.255.109.78:7777/webhook&hub.mode=subscribe&hub.topic=https://www.youtube.com/xml/feeds/videos.xml?channel_id={i}'))
+    print(get(f'https://pubsubhubbub.appspot.com/subscribe?hub.callback={host}&webhook&hub.mode=subscribe&hub.topic=https://www.youtube.com/xml/feeds/videos.xml?channel_id={i}'))

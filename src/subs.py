cfc2484c1f5c4764286aa1fcc653a1021ebfe124
om1ji <om1ji.music@gmail.com>
2022-01-09 14:08:07 +0500

    Delete subs.py

--- src/subs.py
+++
@@ -1,8 +0,0 @@
-from requests import post
-
-host = input("Host: ")
-
-channels = ["UCMSVWxNp1lkEGpDClzm8Qvw"]
-
-for i in channels:
-    print(get(f'https://pubsubhubbub.appspot.com/subscribe?hub.callback={host}&webhook&hub.mode=subscribe&hub.topic=https://www.youtube.com/xml/feeds/videos.xml?channel_id={i}'))
